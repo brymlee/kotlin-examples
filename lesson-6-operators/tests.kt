20f5105fa0f80dd9e95e6023bc1a11fcdb571ab9
brymlee <brymlee1911@gmail.com>
2018-06-18 19:05:20 -0700

    Lesson 6: Operators

---
+++ lesson-6-operators/tests.kt
@@ -0,0 +1,25 @@
+package brymlee
+
+import org.junit.Test
+import org.junit.Assert.*
+import java.io.File
+
+class Tests{
+
+	@Test
+	fun nullFile_doSafeNullReferences(){
+		val nullFile : File? = null
+		val nullExists = nullFile?.exists()
+		val nullFileNameLength = nullFile?.getName()?.length
+		assertNull(nullFile)
+		assertNull(nullExists)
+		assertNull(nullFileNameLength)
+	}
+
+	@Test
+	fun nullInteger_doElvisOperator(){
+		val nullInteger : Int? = null
+		assertEquals(-1, nullInteger ?: -1)
+	}
+
+}
